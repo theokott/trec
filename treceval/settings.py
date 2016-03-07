fa0904d53f01ac8207be6b8a3074357622088cfc
theoakott@gmail.com <2131905K@BO706-35.ad.dcs.gla.ac.uk>
2016-03-07 16:37:28 +0000

    Created index page /trec_eval_app/index

--- treceval/settings.py
+++ treceval/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'trec_eval_app'
 )
 
 MIDDLEWARE_CLASSES = (
--- treceval/urls.py
+++ treceval/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^trec_eval_app/', include('trec_eval_app.urls')),
 )
