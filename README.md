adc41f8c073fa45ca137f0a82f4a7716335ec11c
theoakott@gmail.com <2131905K@BO706-35.ad.dcs.gla.ac.uk>
2016-03-07 17:02:13 +0000

    Created templates folder and updated index page to use templates instead of hardcoded responses

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # trec
 WAD2 TREC evaluator
+
+Remember, the folder and git repository are called trec but the project is called treceval
+and the web app is trec_eval_app
--- trec_eval_app/views.py
+++ trec_eval_app/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("This is the index page for the trec_eval_app")
+    context_dict = {'exampleDjangoVariable': "This is an example of a Django variable!"}
+
+    return render(request, 'trec_eval_app/index.html', context_dict)
