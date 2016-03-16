6961bfd6027c70e9c703406772354da1019e3e1f
theoakott@gmail.com <2131905K@BO706-13.ad.dcs.gla.ac.uk>
2016-03-16 15:45:31 +0000

    created scoreboard and login placeholder pages

--- trec_eval_app/urls.py
+++ trec_eval_app/urls.py
@@ -1,5 +1,8 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from trec_eval_app import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+        url(r'^$', views.index, name='index'),
+        url(r'scoreboard$', views.scoreboard, name='scoreboard'),
+        url(r'login$', views.login, name='scoreboard')
+        )
--- trec_eval_app/views.py
+++ trec_eval_app/views.py
@@ -5,3 +5,12 @@ def index(request):
     context_dict = {'exampleDjangoVariable': "This is an example of a Django variable!"}
 
     return render(request, 'trec_eval_app/index.html', context_dict)
+
+def scoreboard(request):
+    context_dict = {}
+
+    return render(request, 'trec_eval_app/scoreboard.html', context_dict)
+
+def login(request):
+    context_dict = {}
+    return render(request, 'trec_eval_app/login.html', context_dict)
