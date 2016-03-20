cb4275459fb77f325a0af0dfe72c34b43d9ee5a8
Jon Suthakaran <jonathan.suthakaran@gmail.com>
2016-03-20 17:17:34 +0000

    Picture now works on user profile page

--- trec_eval_app/urls.py
+++ trec_eval_app/urls.py
@@ -1,18 +1,17 @@
 from django.conf.urls import patterns, url, include
 from trec_eval_app import views
 
-
-#Takes the stripped URLs passed to it and tries to match what remains
-#If a URL is matched, the given view is called and thus a HTML response is generated
+# Takes the stripped URLs passed to it and tries to match what remains
+# If a URL is matched, the given view is called and thus a HTML response is generated
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'),
-        url(r'scoreboard/$', views.scoreboard, name='scoreboard'),
-        url(r'login/$', views.user_login, name='login'),
-        url(r'^logout/$', views.user_logout, name='logout'),
-        url(r'register/$', views.register, name='register'),
-        url(r'upload/$', views.upload, name='upload'),
-        url(r'user/$', views.user_profile, name='user'),
-        url(r'user/edit/$', views.user_edit, name='edit'),
-        url(r'^scoreboard/(?P<track_slug>[\w\-]+)/$', views.scoreboard, name='scoreboard'),
-        )
+                       url(r'^$', views.index, name='index'),
+                       url(r'scoreboard/$', views.scoreboard, name='scoreboard'),
+                       url(r'login/$', views.user_login, name='login'),
+                       url(r'^logout/$', views.user_logout, name='logout'),
+                       url(r'register/$', views.register, name='register'),
+                       url(r'upload/$', views.upload, name='upload'),
+                       url(r'user/$', views.user_profile, name='user'),
+                       url(r'user/edit/$', views.user_edit, name='edit'),
+                       url(r'^scoreboard/(?P<track_slug>[\w\-]+)/$', views.scoreboard, name='scoreboard'),
+                       )
--- treceval/urls.py
+++ treceval/urls.py
@@ -2,16 +2,19 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 
+# Examples:
+# url(r'^$', 'treceval.views.home', name='home'),
+# url(r'^blog/', include('blog.urls')),
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'treceval.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^trec_eval_app/', include('trec_eval_app.urls')),     #Takes urls starting with /trec_eval_app/..., strips /trec_eval_app/ and passes the rest to trec_eval_app.urls be matched
-    url(r'^$', include('trec_eval_app.urls'))
-)
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^trec_eval_app/', include('trec_eval_app.urls')),
+                       url(r'^$', include('trec_eval_app.urls'))
+                       )
+# Takes urls starting with /trec_eval_app/..., strips /trec_eval_app/
+# and passes the rest to trec_eval_app.urls be matched
 
 if settings.DEBUG:
-    urlpatterns += patterns('django.views.static', (r'^media/(?P<path>.*)', 'serve',
-                            {'document_root': settings.MEDIA_ROOT}), )
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns('',
+                            (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                             {'document_root': settings.MEDIA_ROOT}))
