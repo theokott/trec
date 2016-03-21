994e4f6ab32c18c5e3479154cb8ea9355c816bee
singhe <eriksingh15@gmail.com>
2016-03-21 23:31:14 +0000

    Comments to test cases class.

--- unit_testing.py
+++ unit_testing.py
@@ -6,13 +6,16 @@ import unittest
 
 from trec_eval_app.models import Track, Task, UserProfile, Run
 
+#				Class TestTrecEvaluator
+#	This class implements the unit testing methodology.
+#	Two test cases are implemented on the populate_trec.py script.
 
 class TestTrecEvaluator(unittest.TestCase):
 
-    def test_add_track(self):
+    def test_add_track(self):  #Test testing the insertion of a track.
         self.assertEqual((add_track(0,"testTrack1","First unit test: this Track tests the method add_track")).name,"testTrack1")
 
-    def test_add_user(self):
+    def test_add_user(self):   #Test testing the insertion of a user.
         self.assertEqual((add_user("testUser1", "test", "user", "test@test.com", "SecondUnitTest")).username,"testUser1")
 
 
